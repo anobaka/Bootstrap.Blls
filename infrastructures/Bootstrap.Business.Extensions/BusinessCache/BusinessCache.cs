aec4b1f47dbe8806d25f887dca0ed17f618ab25e
Anobaka <anobaka@qq.com>
2019-03-12 11:04:18 +0800

    update

---
+++ infrastructures/Bootstrap.Business.Extensions/BusinessCache/BusinessCache.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Bootstrap.Business.Extensions.BusinessCache
+{
+    /// <summary>
+    /// TODO: This feature is used with business handler, putting it here temporarily until the stable business handler borns.
+    /// </summary>
+    public class BusinessCache
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelFullnames">Assembly - Fullnames</param>
+        public static void Init(Dictionary<string, IEnumerable<string>> modelFullnames)
+        {
+            foreach (var ak in modelFullnames)
+            {
+                var assembly = Assembly.Load(ak.Key);
+                if (ak.Value != null)
+                {
+                    foreach (var f in ak.Value)
+                    {
+                        var type = typeof(BusinessCacheOptions<>).MakeGenericType(
+                            assembly.GetType(f));
+                        type.GetProperty("Enabled").SetValue(null, true);
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="resources">AssembleName - Namespace - Model names</param>
+        public static void Init(Dictionary<string, Dictionary<string, IEnumerable<string>>> resources) => Init(
+            resources.ToDictionary(t => t.Key, t => t.Value.SelectMany(a => a.Value.Select(b => $"{a.Key}.{b}"))));
+    }
+}
---
+++ infrastructures/Bootstrap.Business.Extensions/BusinessCache/BusinessCacheOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Bootstrap.Business.Extensions.BusinessCache
+{
+    /// <summary>
+    /// <see cref="BusinessCache"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BusinessCacheOptions<T>
+    {
+        public static bool Enabled { get; set; }
+    }
+}
