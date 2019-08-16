97cc24895317bde537049f990375c2ea4b3cd7e3
Jax <jax.zhu@aihuishou.com>
2019-08-16 18:19:09 +0800

    update

---
+++ infrastructures/Bootstrap.Business.Components/Extensions/MockExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bootstrap.Business.Components.Services.Infrastructures;
+using Bootstrap.Infrastructures.Extensions;
+using Bootstrap.Infrastructures.Models.ResponseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bootstrap.Business.Components.Extensions
+{
+    public static class MockExtensions
+    {
+        public static async Task<T> GetOrCreate<TDbContext, T>(AbstractService<TDbContext, T> service,
+            Expression<Func<T, bool>> first,
+            object createModel) where T : class where TDbContext : DbContext
+
+        {
+            var instance = await service.GetFirst(first);
+            if (instance == null)
+            {
+                var serviceType = service.GetType();
+                var methods = serviceType.GetMethods();
+                var method = methods
+                                 .FirstOrDefault(t =>
+                                     t.Name.Equals("Create") && t.GetParameters().FirstOrDefault()?.ParameterType.Name
+                                         .Contains("CreateRequestModel") == true) ??
+                             methods
+                                 .FirstOrDefault(t =>
+                                     t.Name.Equals("Create") && t.GetParameters().FirstOrDefault()?.ParameterType.Name
+                                         .Equals(SpecificTypeUtils<T>.Type.Name) == true);
+                instance = (await (method.Invoke(service, new[] {createModel}) as Task<SingletonResponse<T>>)).Data;
+            }
+
+            return instance;
+        }
+    }
+}
