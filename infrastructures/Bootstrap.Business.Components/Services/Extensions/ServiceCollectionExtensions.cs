311a78643fe1aa7b51b83c2975597291df8448d1
Jax <jax.zhu@aihuishou.com>
2019-09-05 23:59:19 +0800

    update

---
+++ infrastructures/Bootstrap.Business.Components/Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bootstrap.Business.Components.Services.Infrastructures;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Bootstrap.Business.Components.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBootstrapServices<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
+        {
+            services.TryAddSingleton<BaseService<TDbContext>>();
+            return services;
+        }
+    }
+}
